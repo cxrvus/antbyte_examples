296332e6f266971b6e74017e10f27da0bb5fed4a
cxrvus <benito.m.rabe@gmail.com>
2026-03-04 20:01:25 +0100

    langton.and.mjs progress

---
+++ langton.ant.mjs
@@ -0,0 +1,28 @@
+/// <reference path="../js/lib.d.ts" />
+
+import { ant } from "../js/lib.mjs"
+
+/** @type {World} */
+const world = {
+	cfg: {
+		width: 64,
+		height: 64,
+		speed: 64,
+		looping: false,
+		border_mode: "despawn",
+		starting_pos: "center",
+		color_mode: "binary",
+		hide_title: false,
+		description: "Langton's Ant",
+	},
+	ants: {
+		1: ant("main", (C2) => ({
+			CC: true,
+			C2: !C2,
+			D1: true,
+			D2: C2,
+		})),
+	},
+}
+
+console.log(JSON.stringify(world, null, '\t'))
